5db3668028a8350c86036fd4caef629ef282b019
Akobir Tursunov <akobirtursunov30@gmail.com>
2022-11-07 19:09:02 +0500

    initial commit

--- js/main.js
+++ js/main.js
@@ -1,191 +1,126 @@
-
 let quizData = [
-    {
-        question: 'Who is the President of Uzb?',
-        a:'Tursunov Akobir',
-        b:'Jahongir Tursunov',
-        c:'Khurshidbek Ruziyev',
-        d:'Shavkat Mirziyoyev',
-        correct:'d' 
-    },
-    {
-        question: 'How old is Akobir?',
-        a:16,
-        b:17,
-        c:15,
-        d:18,
-        correct:'a' 
-    },
-    {
-        question: 'Which programming language is the most famous?',
-        a:'Python',
-        b:'Java',
-        c:'Javascript',
-        d:'C++',
-        correct:'c'
-    },
-    {
-        question: 'When launched Javascript?',
-        a:1994,
-        b:1995,
-        c:1996,
-        d:'Answer not given',
-        correct:'b' 
-    },
-    {
-        question: 'Where is located Cambridge?',
-        a:'in USA',
-        b:'in Canada',
-        c:'in Great Britain',
-        d:'in Uzbekistan',
-        correct:'c' 
-    }
+  {
+    question: "Who is the President of Uzb?",
+    a: "Tursunov Akobir",
+    b: "Jahongir Tursunov",
+    c: "Khurshidbek Ruziyev",
+    d: "Shavkat Mirziyoyev",
+    correct: "d",
+  },
+  {
+    question: "How old is Akobir?",
+    a: 16,
+    b: 17,
+    c: 15,
+    d: 18,
+    correct: "a",
+  },
+  {
+    question: "Which programming language is the most famous?",
+    a: "Python",
+    b: "Java",
+    c: "Javascript",
+    d: "C++",
+    correct: "c",
+  },
+  {
+    question: "When launched Javascript?",
+    a: 1994,
+    b: 1995,
+    c: 1996,
+    d: "Answer not given",
+    correct: "b",
+  },
+  {
+    question: "Where is located Cambridge?",
+    a: "in USA",
+    b: "in Canada",
+    c: "in Great Britain",
+    d: "in Uzbekistan",
+    correct: "c",
+  },
 ];
-let startBtn = document.querySelector('.start');
-let rules = document.querySelector('.rules');
-let exitBtn = document.querySelector('.exit'); 
-let contBtn = document.querySelector('.cont'); 
-let quizContainer = document.querySelector('#quiz');
-let questionTitle  = document.querySelector('#question_title');
-let aText = document.querySelector('#a_text');
-let bText = document.querySelector('#b_text');
-let cText = document.querySelector('#c_text');
-let dText = document.querySelector('#d_text');
-let answerEls = document.querySelectorAll('.answer');
-let btn = document.querySelector('.submit');
+let startBtn = document.querySelector(".start");
+let rules = document.querySelector(".rules");
+let exitBtn = document.querySelector(".exit");
+let contBtn = document.querySelector(".cont");
+let quizContainer = document.querySelector("#quiz");
+let questionTitle = document.querySelector("#question_title");
+let aText = document.querySelector("#a_text");
+let bText = document.querySelector("#b_text");
+let cText = document.querySelector("#c_text");
+let dText = document.querySelector("#d_text");
+let answerEls = document.querySelectorAll(".answer");
+let btn = document.querySelector(".submit");
 let count = 0;
 let correct = 0;
 let qs_count = 1;
 let time = 15;
 
-document.querySelector('.qs_count').innerHTML = `<h5>${qs_count} of ${quizData.length} Questions</h5>`
+document.querySelector(
+  ".qs_count"
+).innerHTML = `<h5>${qs_count} of ${quizData.length} Questions</h5>`;
 
-function countdown(){
-    document.querySelector('.time').innerHTML = time;
-    time--;
-    if(time < 0){
-        time = 0;
-    }
-    time = time < 10 ? `0${time}` : time;
-    return time;
-}
-setInterval(countdown,1000); 
-
-function showQuestions(){
-    deselect();
-    questionTitle.textContent = quizData[count].question;
-    aText.textContent = quizData[count].a;
-    bText.textContent = quizData[count].b;
-    cText.textContent = quizData[count].c;
-    dText.textContent = quizData[count].d;
-};
-
-function getSelected(){
-    let answer;
-    answerEls.forEach(answerEl => {
-        if(answerEl.checked) answer = answerEl.id;
-    })
-    return answer;
+function showQuestions() {
+  deselect();
+  questionTitle.textContent = quizData[count].question;
+  aText.textContent = quizData[count].a;
+  bText.textContent = quizData[count].b;
+  cText.textContent = quizData[count].c;
+  dText.textContent = quizData[count].d;
 }
 
-function deselect(){
-         answerEls.forEach(answerEl => {
-             answerEl.checked = false;
-            }
-            )}
-                   
-startBtn.addEventListener('click',()=>{
-    startBtn.style.display = 'none';
-    rules.style.display = 'block';
+function getSelected() {
+  let answer;
+  answerEls.forEach((answerEl) => {
+    if (answerEl.checked) answer = answerEl.id;
+  });
+  return answer;
+}
 
-})
+function deselect() {
+  answerEls.forEach((answerEl) => {
+    answerEl.checked = false;
+  });
+}
 
-exitBtn.addEventListener('click',()=> {
-    rules.style.display = 'none';
-    startBtn.style.display = 'block';
-})
+startBtn.addEventListener("click", () => {
+  startBtn.style.display = "none";
+  rules.style.display = "block";
+});
 
-contBtn.addEventListener('click',()=>{
-    rules.style.display = 'none';
-    quizContainer.style.display = 'block';
-})
+exitBtn.addEventListener("click", () => {
+  rules.style.display = "none";
+  startBtn.style.display = "block";
+});
 
-document.addEventListener('DOMContentLoaded',() => {
-    showQuestions();
-})
+contBtn.addEventListener("click", () => {
+  rules.style.display = "none";
+  quizContainer.style.display = "block";
+});
 
+document.addEventListener("DOMContentLoaded", () => {
+  showQuestions();
+});
 
-btn.addEventListener('click',() =>{
-    time = 15;
-    countdown();
-    let answer = getSelected();
-    if(answer){
-        if(answer == quizData[count].correct) correct++;
-    }
-    count++;
-    
-    qs_count++;
-    document.querySelector('.qs_count').innerHTML = `<h5>${qs_count} of ${quizData.length} Questions</h5>`
-    
-    if(count < quizData.length) showQuestions();
-    else quizContainer.innerHTML = `<p>Your result: <br><h1>${correct}/${quizData.length}</h1><br></p>`
-})
+btn.addEventListener("click", () => {
+  time = 15;
+  countdown();
+  let answer = getSelected();
+  if (answer) {
+    if (answer == quizData[count].correct) correct++;
+  }
+  count++;
 
+  qs_count++;
+  document.querySelector(
+    ".qs_count"
+  ).innerHTML = `<h5>${qs_count} of ${quizData.length} Questions</h5>`;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  if (count < quizData.length) showQuestions();
+  else
+    quizContainer.innerHTML = `<p>Your result: <br><h1>${correct}/${quizData.length}</h1><br></p>`;
+});
 
 // let question = document.getElementById('question_title');
 // let a_text = document.getElementById('a_text');
@@ -203,12 +138,11 @@ btn.addEventListener('click',() =>{
 //     deselect();
 //    let currentQuizData = quizData[currentQuiz];
 //     question.innerText = currentQuizData.question;
-//     a_text.innerText = currentQuizData.a;    
+//     a_text.innerText = currentQuizData.a;
 //     b_text.innerText = currentQuizData.b;
 //     c_text.innerText = currentQuizData.c;
 //     d_text.innerText = currentQuizData.d;
 // }
-
 
 // function getSelected(){
 //     let answer = undefined;
@@ -228,10 +162,8 @@ btn.addEventListener('click',() =>{
 //     }
 // )}
 
-
-
 // submitBtn.addEventListener('click',() => {
-   
+
 //   const answer =getSelected();
 //    if(answer){
 //         if(answer === quizData[currentQuiz].correct){
@@ -241,9 +173,9 @@ btn.addEventListener('click',() =>{
 //     }
 //     currentQuiz++;
 //     if(currentQuiz < quizData.length ){
-//         loadQuiz(); 
+//         loadQuiz();
 //         }
 //     else{
 //        document.getElementById('quiz').innerHTML = `<p>Your result: <br> <h1> ${score}/${quizData.length} </h1>`;
-//     }   
+//     }
 // })
